f2c4c69b8f4ba74337ce4591b088198d4debec6e
Cathrin <cathrinmariya21@gmail.com>
2025-05-15 14:26:49 +0530

    Re-uploading correct project files

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,16 +2,14 @@ terraform {
   required_providers {
     local = {
       source = "hashicorp/local"
-      version = "~> 2.0"
     }
   }
-
-  required_version = ">= 1.0.0"
 }
 
-provider "local" {}
+provider "local" {
+}
 
-resource "local_file" "hello_file" {
-  filename = "hello.txt"
-  content  = "Terraform setup successful! 🎉"
+resource "local_file" "simulated_vm" {
+  filename = "${path.module}/vm.txt"
+  content  = "This represents a virtual machine provisioned using Terraform."
 }
